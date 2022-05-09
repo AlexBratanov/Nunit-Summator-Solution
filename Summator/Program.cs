8da07736d5b2bbc66e06daf5eb87868585f6f0f7
AlexBratanov <105238673+AlexBratanov@users.noreply.github.com>
2022-05-10 00:31:38 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Summator.Sum(new int[] { 1, 2, 3 });
+            if (sum == 6)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            int average = Summator.Average(new int[] { 5, 10, 15 });
+            if (average == 10)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
+          
